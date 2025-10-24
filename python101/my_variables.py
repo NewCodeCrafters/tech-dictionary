879ca83515019694341cec57658b84267bab0d9f
peterbabatunde-sys <peterbabatunde456@gmail.com>
2025-10-24 22:22:23 +0100

    Remove python101 folder

--- python101/my_variables.py
+++
@@ -1,9 +0,0 @@
-# mystring = "string"
-# mynumberr = 45
-# myfloat = 45.6
-# ismarried = True
-
-# print(type(mystring))
-# print(type(mynumberr))
-# print(type(myfloat))
-# print(type(ismarried))
--- python101/saymyname.py
+++
@@ -1,8 +0,0 @@
-import time
-
-name = input("What is your name:? ").title()
-repeat = int(input(f"{name}, how many times can i say your name?: "))
-for i in range(repeat):
-    print(f"Your name is {name}.")
-    time.sleep(1)
-print(f"I just called your name {repeat} times.") 
